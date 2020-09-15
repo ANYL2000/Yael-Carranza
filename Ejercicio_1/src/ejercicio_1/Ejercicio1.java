4598e4478ee18c8cbd2353e0fdd952e4d389de0d
ANYL2000 <70918693+ANYL2000@users.noreply.github.com>
2020-09-15 14:40:06 -0500

    ruleta de participacion1.0

--- Ejercicio_1/src/ejercicio_1/Ejercicio1.java
+++ Ejercicio_1/src/ejercicio_1/Ejercicio1.java
@@ -9,14 +9,51 @@ package ejercicio_1;
  *
  * @author anylc
  */
-public class Ejercicio_1 {
+public class Ejercicio1 {
+ int tope;
+ Datos nombre [] = new Datos[43];
+Scanner entrada= new Scanner(System.in);
 
+    public void capturarDatos(){
+        if(tope>=5){
+            
+            System.out.println("ERROR");
+            
+        }else{
+            
+            nombre [tope] =new Datos();
+            System.out.println("Dame el nombre del alumno");
+            nombre[tope].nombre= entrada.nextLine();
+             System.out.println("Dame el numero de lista del alumno");
+            nombre[tope].N_lista= entrada.nextInt();
+            
+            nombre[tope].control=false;
+            tope++;
+            
+        }
+        
+        
+        
+    }
+    
+     public void MostrarDatos(){
+        
+        for(int i=0;i<=tope;i++){
+            
+            System.out.println("Nombre del Alumno: "+nombre[i].nombre);
+             System.out.println("Numero de lista del Alumno: "+nombre[i].N_lista);
+        }
+        
+        
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo NetBeans");
-         System.out.println("Hola Mundo GitHub");
+        
+         System.out.println("Programa de ruleta de participacion");
+     
+        
     }
     
 }
