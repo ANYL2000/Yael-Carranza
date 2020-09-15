72cb1c7fc2ffa79da90c839180473458c6b34443
anylc <anylc@DESKTOP-8OR1T13.mshome.net>
2020-09-15 12:54:53 -0500

    Creando commit desde netbeans2

--- Ejercicio_1/src/ejercicio_1/Ejercicio_1.java
+++ Ejercicio_1/src/ejercicio_1/Ejercicio_1.java
@@ -15,7 +15,7 @@ public class Ejercicio_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo NetBeans");
+        System.out.println("Hola Mundo NetBeans2");
     }
     
 }
