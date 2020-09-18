a3985d83361b896b7fd1d58a0b4e084c430c9885
ANYL2000 <70918693+ANYL2000@users.noreply.github.com>
2020-09-18 10:38:55 -0500

    terminado ejercicio de la ruleta de participacion

--- Ejercicio_1_ruleta/src/ejercicio_1_ruleta/Ejercicio_1_ruleta.java
+++ Ejercicio_1_ruleta/src/ejercicio_1_ruleta/Ejercicio_1_ruleta.java
@@ -12,7 +12,9 @@ import java.util.Scanner;
  * @author anylc
  */
 public class Ejercicio_1_ruleta {
- int tope;
+ int tope=0;
+ 
+ 
  Datos nombre [] = new Datos[43];
 Scanner entrada= new Scanner(System.in);
 
@@ -23,7 +25,10 @@ Scanner entrada= new Scanner(System.in);
             
         }else{
             
+            System.out.println("\nAGREGAR DATOS\n");
+            
             nombre [tope] =new Datos();
+            entrada.nextLine();
             System.out.println("Dame el nombre del alumno");
             nombre[tope].nombre= entrada.nextLine();
              System.out.println("Dame el numero de lista del alumno");
@@ -39,26 +44,84 @@ Scanner entrada= new Scanner(System.in);
     }
     
      public void MostrarDatos(){
-        
-        for(int i=0;i<=tope;i++){
+         System.out.println("\nMOSTRAR DATOS\n");
+        for(int i=0;i<tope;i++){
             
             System.out.println("Nombre del Alumno: "+nombre[i].nombre);
              System.out.println("Numero de lista del Alumno: "+nombre[i].N_lista);
+             System.out.println("\n");
         }
+     } 
         
         
+        public void menu(){
+            String volver="";
+            do{
+           String opcion=""; 
+           do{
+           System.out.println("Presione");
+          System.out.println("1 para agregar alumno");
+           System.out.println("2 para mostrar alumno");
+          System.out.println("3 para salir");
+         opcion=entrada.next();
+           }while(!opcion.equals("1")&&!opcion.equals("2")&&!opcion.equals("3")); 
+           
+        switch(opcion){
+            
+            case "1":
+                
+                capturarDatos();
+                
+                break;
+                
+            case "2":
+                MostrarDatos();
+                
+                
+                break;
+            
+                
+            case "3":  break;
+                    
+            
+        }   
+            
+             System.out.println("\nDesea volver?\n\n(pulsa 1) para SI\n¡¡pulsa cualquier tecla para NO!!\n");
+         volver = entrada.next();
+        
+       }while(volver.equals("1")); 
+        
+        System.out.println("\n¡¡ADIOS!!\n");     
+     
     }
+        
+        
+        
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         System.out.println("Programa de ruleta de participacion");
+   
+        Ejercicio_1_ruleta llamar= new Ejercicio_1_ruleta();
+        
+         System.out.println("Programa de ruleta de participacion\n");
          
-         System.out.println("Presione");
-          System.out.println("1 para agregar alumno");
-           System.out.println("2 para mostrar alumno");
          
+        
+    
+        llamar.menu();
+          
+          
+          
+          
+          
+          
+          
+          
          
     }
+    
+    
+   
     
 }
